439de7d221088488dbc9fb92b0d944f36e97e45f
Kotikhin Alex <sapool@bk.ru>
2025-06-09 20:59:21 +0300

    tg auth

---
+++ lib/telegram.ts
@@ -0,0 +1,11 @@
+import WebApp from '@twa-dev/sdk';
+
+export function initTelegramWebApp() {
+    WebApp.ready();
+
+    return {
+        user: WebApp.initDataUnsafe.user,
+        version: WebApp.version,
+        platform: WebApp.platform,
+    };
+}
--- providers/ReduxProvider.tsx
+++ providers/ReduxProvider.tsx
@@ -1,9 +1,18 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
+import { initTelegramWebApp } from '@/lib/telegram';
 
 export default function ReduxProvider({ children }: { children: ReactNode }) {
+    useEffect(() => {
+        const tgData = initTelegramWebApp();
+        console.log('Telegram User Data:', tgData.user);
+        // Здесь можно добавить логику проверки/создания пользователя в БД
+    }, []);
+
+
+
     return <Provider store={store}>{children}</Provider>;
 }
